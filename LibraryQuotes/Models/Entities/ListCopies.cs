6e171e9a7a5160825c5b842e49ab00c754eed58c
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-01 18:54:56 -0500

    Add copy list entity

---
+++ LibraryQuotes/Models/Entities/ListCopies.cs
@@ -0,0 +1,20 @@
+﻿namespace LibraryQuotes.Models.Entities
+{
+    public class ListCopies
+    {
+        public List<Copy> copies {  get; set; }
+        public float Total { get; set; }
+        public float TotalDiscount { get; set; }
+
+        public ListCopies()
+        {
+        }
+
+        public ListCopies(List<Copy> copies, float total, float totalDiscount)
+        {
+            this.copies = copies;
+            Total = total;
+            TotalDiscount = totalDiscount;
+        }
+    }
+}
