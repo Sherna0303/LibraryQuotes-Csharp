19c015d32c67057642bc68bb5d29fe482ed16542
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-01 18:18:14 -0500

    Add application controller

---
+++ LibraryQuotes/Controllers/QuotesController.cs
@@ -0,0 +1,24 @@
+﻿using LibraryQuotes.Models.DTOS;
+using LibraryQuotes.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryQuotes.Controllers
+{
+    [ApiController]
+    [Route("/api")]
+    public class QuotesController : ControllerBase
+    {
+        private readonly IQuotesService _quotesService;
+
+        public QuotesController(IQuotesService quotesService)
+        {
+            _quotesService = quotesService;
+        }
+
+        [HttpPost("/calculateCopyPrice")]
+        public async Task<IActionResult> calculateCopyPrice(CopyDTO payload)
+        {
+            return StatusCode(StatusCodes.Status200OK, _quotesService.CalculatePrice(payload));
+        }
+    }
+}
