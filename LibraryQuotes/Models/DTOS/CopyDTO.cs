65104e7f36d553996230a2871aef0cb31cd2916f
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-01 15:18:49 -0500

    Add DTO for copies

---
+++ LibraryQuotes/Models/DTOS/CopyDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace LibraryQuotes.Models.DTOS
+{
+    public class CopyDTO
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public float precio { get; set; }
+
+        public CopyDTO()
+        {
+        }
+
+        public CopyDTO(string name, string author, float precio)
+        {
+            Name = name;
+            Author = author;
+            this.precio = precio;
+        }
+    }
+}
