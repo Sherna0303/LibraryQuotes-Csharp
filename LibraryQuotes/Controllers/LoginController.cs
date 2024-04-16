d22a8c1ab1332f21ece74ea4894835bee26046d9
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-15 19:49:26 -0500

    Add new LoginController with Login endpoint

---
+++ LibraryQuotes/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using LibraryQuotes.Models.DTOS;
+using LibraryQuotes.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryQuotes.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService _loginService;
+
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Login(UserDTO userDTO)
+        {
+            var user = await _loginService.GetUser(userDTO);
+
+            if (user is null)
+            {
+                return BadRequest(new { message = "Credenciales invalidas" });
+            }
+
+            return Ok(new { token = "Token" });
+        }
+    }
+}
