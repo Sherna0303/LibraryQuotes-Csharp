0837952d6313c64a34d971a7d336d330c0590eea
Sebastian_Hernandez_Patio <sebastianhernandezpatio.2004@gmail.com>
2024-04-15 19:47:10 -0500

    Add models for the User

---
+++ LibraryQuotes/Models/DTOS/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryQuotes.Models.DTOS
+{
+    public class UserDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int AntiquityYears { get; set; }
+    }
+}
